8436caac591463751583b0a6773b2757870fc491
Tom_Tenv <vdemotz@student.ethz.ch>
2014-05-15 13:11:38 +0200

    Interface of database
    Added first draw of database interface into
    sqlRevisited/Connection.java
    Methods:
    +min()
    +get()
    +succ()
    +delete()
    +insert()
    +update()
    To be discussed.
    
    Added .gitignore for the database. Hope it works this time.

--- src/ch/ethz/inf/dbproject/sqlRevisited/Connection.java
+++ src/ch/ethz/inf/dbproject/sqlRevisited/Connection.java
@@ -17,5 +17,64 @@ public class Connection {
 	public PreparedStatement prepareStatement(String stringQuery){
 		return null;
 	}
+	
+	/**
+	 * Return the first object of the given table name
+	 * @param tableName a table name of the database
+	 * @return the first Object of this table
+	 */
+	public Object min(String tableName){
+		return null;
+	}
+	
+	/**
+	 * Get a particular object of a table given a primary key
+	 * @param primaryKeys which identify the object
+	 * @param tableName a table name of the database
+	 * @return the object with given primary keys
+	 */
+	public Object get(Object[] primaryKeys, String tableName){
+		return null;
+	}
+	
+	/**
+	 * Get the successor of a particular object of a table given a primary key
+	 * @param primaryKeys which identify the predecessor of returned object
+	 * @param tableName a table name of the database
+	 * @return the successor of the object with given primary keys
+	 */
+	public Object succ(Object[] primaryKeys, String tableName){
+		return null;
+	}
+	
+	/**
+	 * Delete an object from a table if exists given a primary key
+	 * @param primaryKeys which identify the predecessor of returned object
+	 * @param tableName a table name of the database
+	 * @return true if object has been deleted, else false
+	 */
+	public boolean delete(Object[] primaryKeys, String tableName){
+		return false;
+	}
 
+	/**
+	 * Insert an object into a table
+	 * @param toInsert object to be inserted
+	 * @param tableName a table name of the database
+	 * @return true if object has been inserted, else false
+	 */
+	public boolean insert(Object toInsert, String tableName){
+		return false;
+	}
+	
+	/**
+	 * Update an object of a table
+	 * @param primaryKeys which identify the object to update
+	 * @param toUpdate object to be updated 
+	 * @param tableName a table name of the database
+	 * @return true if object has been updated, else false
+	 */
+	public boolean update(Object[] primaryKeys, Object toUpdate, String tableName){
+		return false;
+	}
 }
