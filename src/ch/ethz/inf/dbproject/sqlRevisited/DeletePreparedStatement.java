35f6163d71e797613bdb598283df79c16e65626c
Tom_Tenv <vdemotz@student.ethz.ch>
2014-05-23 13:14:12 +0200

    Github bug

---
+++ src/ch/ethz/inf/dbproject/sqlRevisited/DeletePreparedStatement.java
@@ -0,0 +1,19 @@
+package ch.ethz.inf.dbproject.sqlRevisited;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.ArrayList;
+
+import ch.ethz.inf.dbproject.sqlRevisited.Parser.SQLToken;
+
+public class DeletePreparedStatement  extends AbstractPreparedStatement {
+
+	/**
+	 * Create a new PrepareStatement update
+	 * @param deleteTokensStream
+	 */
+	public DeletePreparedStatement(ArrayList<SQLToken> deleteTokensStream){
+		
+	}
+}
